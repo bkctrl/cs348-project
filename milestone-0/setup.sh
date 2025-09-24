9e6eb281e274860d803f744b023f7815001e8c1a
bkctrl <hb3kang@uwaterloo.ca>
2025-09-24 15:45:13 -0400

    added setup.sh, to be completed

--- milestone-0/setup.sh
+++ milestone-0/setup.sh
@@ -3,7 +3,21 @@
 # CS348 Project - Milestone 0 Setup Script
 #
 
-sudo apt update -y
-sudo apt install -y python3-pip mysql-client
-pip3 install --user pandas
+set -e
 
+DB_NAME=cs-348
+DB_USER=current-user
+echo "Enter your MySQL password (leave blank if none):"
+read -rs DB_PASS
+
+MYSQL_CMD="mysql -u $DB_USER"
+if [ -n "$DB_PASS" ]; then
+  MYSQL_CMD="$MYSQL_CMD -p$DB_PASS"
+fi
+
+echo "Creating database '$DB_NAME'..."
+$MYSQL_CMD -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+echo "Database '$DB_NAME' created successfully!"
+echo ""
+echo "You can connect to it with:"
+echo "  mysql -u $DB_USER -p $DB_NAME"
